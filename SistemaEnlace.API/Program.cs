16a737f53dbeeafa414d793d63e0c066b031a122
TABORDA <USUARIO@DESKTOP-HN7KPIJ>
2024-05-19 15:01:15 -0500

    Revert "1"
    This reverts commit b292e94b8ccf71cc848e1deb0ede9522d26cbd70.

--- SistemaEnlace.API/Program.cs
+++ SistemaEnlace.API/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using SistemaEnlace.API.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,7 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
@@ -25,12 +21,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.UseCors(x => x
-    .AllowAnyHeader()
-    .AllowAnyMethod()
-    .SetIsOriginAllowed(origin => true)
-    .AllowCredentials());
-
 
 app.Run();
