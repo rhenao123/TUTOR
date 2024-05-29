e567f3d10f727902beb6acd04ca3b1a579246799
ROBINSON DIONISIO HENAO NORIEGA <robinsonhenao328617@correo.itm.edu.co>
2024-05-28 21:27:40 -0500

    Usuarios

--- SistemaEnlace.API/Program.cs
+++ SistemaEnlace.API/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
 using Microsoft.EntityFrameworkCore;
 using SistemaEnlace.API.Data;
+using SistemaEnlace.API.Helpers;
+using SistemaEnlace.Shared.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +12,42 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
 
+//Meter un builder services
+builder.Services.AddIdentity<User, IdentityRole>(x =>
+{
+    x.User.RequireUniqueEmail = true;
+    x.Password.RequireDigit = false;   //Esto es lo que requiere una contraseña, falso es para crearla normal
+    x.Password.RequiredUniqueChars = 0;
+    x.Password.RequireUppercase = false;
+    x.Password.RequireLowercase = false;
+    x.Password.RequireNonAlphanumeric = false;
+
+}).AddEntityFrameworkStores<DataContext>()
+.AddDefaultTokenProviders();
+
+
+builder.Services.AddScoped<IUserHelper, UserHelper>();
+
+
+
+
+//Fin de los builder metidos
+builder.Services.AddSwaggerGen(); builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
+
+builder.Services.AddTransient<SeedDb>();
 var app = builder.Build();
+SeedData(app);
+void SeedData(WebApplication app)
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory!.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,15 +58,27 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+//Antes de autorizacion meter este
 app.UseAuthorization();
 
 app.MapControllers();
 
+
 app.UseCors(x => x
-    .AllowAnyMethod()
-    .AllowAnyHeader()
-    .SetIsOriginAllowed(origin => true)
-    .AllowCredentials());
+
+.AllowAnyMethod()
+.AllowAnyHeader()
+.AllowCredentials()
+.SetIsOriginAllowed(origin => true)
+
+);
+
+
+
+
+
+
 
 
 
