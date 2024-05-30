67058c78f3f814544295672dddbd9ae2fba94442
TABORDA <USUARIO@DESKTOP-HN7KPIJ>
2024-05-30 16:18:56 -0500

    comit a mitad de precio $$

---
+++ SistemaEnlace.WEB/Auth/AuthenticationProviderTes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+
+            var anonimous = new ClaimsIdentity();
+            var userJ = new ClaimsIdentity(new List<Claim>
+        {
+            new Claim("FirstName", "Robin"),
+            new Claim("LastName", "Dionisio"),
+            new Claim(ClaimTypes.Name, "Dioni@Fatalerror.com"),
+            new Claim(ClaimTypes.Role, "Admin")
+        },
+
+                authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(userJ)));
+        }
+    }
+}
