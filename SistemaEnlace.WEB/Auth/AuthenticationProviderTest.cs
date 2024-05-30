2232c8ba8618a1eca9ee3ec076a12c6fb15c7008
TABORDA <USUARIO@DESKTOP-HN7KPIJ>
2024-05-30 10:10:41 -0500

    comit que va a destruir toda mi existencia

---
+++ SistemaEnlace.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+        }
+    }
+}
